e2ecadcccd0ef21a0fe2d1f50edb5823965e3f15
seals55 <seals55@users.noreply.github.com>
2022-06-21 09:59:12 -0400

    test rclone version

--- Utils/get_rclone.sh
+++ Utils/get_rclone.sh
@@ -1,16 +1,12 @@
 # /usr/bin/env bash
 
+# get latest rclone
 curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
 unzip rclone-current-linux-amd64.zip
-# cd rclone-*-linux-amd64
 
+# find downloaded rclone
 rclone=$(realpath "$(find . -type f -name rclone)")
 echo "Found RCLONE: \"${rclone}\""
+chmod 755 "${rclone}"
 
-# sudo cp rclone /usr/bin/
-# sudo chown root:root /usr/bin/rclone
-# sudo chmod 755 /usr/bin/rclone
-
-# sudo mkdir -p /usr/local/share/man/man1
-# sudo cp rclone.1 /usr/local/share/man/man1/
-# sudo mandb
+"${rclone}" version
