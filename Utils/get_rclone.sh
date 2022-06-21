50b6140e129b1d1ba375b4e5087e77732d4a729a
seals55 <seals55@users.noreply.github.com>
2022-06-21 09:50:39 -0400

    download rclone test

--- Utils/get_rclone.sh
+++ Utils/get_rclone.sh
@@ -1,4 +1,15 @@
 # /usr/bin/env bash
 
-ls -la
+curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
+unzip rclone-current-linux-amd64.zip
+# cd rclone-*-linux-amd64
 
+find . -print | grep rclone
+
+# sudo cp rclone /usr/bin/
+# sudo chown root:root /usr/bin/rclone
+# sudo chmod 755 /usr/bin/rclone
+
+# sudo mkdir -p /usr/local/share/man/man1
+# sudo cp rclone.1 /usr/local/share/man/man1/
+# sudo mandb
