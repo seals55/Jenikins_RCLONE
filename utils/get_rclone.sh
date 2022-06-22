d6a07aca6bf4f8a1ceb35530ef1e451d268646c3
seals55 <seals55@users.noreply.github.com>
2022-06-21 23:29:46 -0400

    rclone website down

--- utils/get_rclone.sh
+++ utils/get_rclone.sh
@@ -1,8 +1,11 @@
 # /usr/bin/env bash
 
 # get latest rclone
-curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
-unzip rclone-current-linux-amd64.zip
+# curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
+# unzip rclone-current-linux-amd64.zip
+
+curl -O https://github.com/rclone/rclone/releases/download/v1.58.1/rclone-v1.58.1-linux-amd64.zip
+unzip rclone-v1.58.1-linux-amd64.zip
 
 # find downloaded rclone
 rclone=$(realpath "$(find . -type f -name rclone)")
