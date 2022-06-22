69565767ef6ac0ca512c6deee92c342ba0b201dc
seals55 <seals55@users.noreply.github.com>
2022-06-21 23:37:38 -0400

    fix curl

--- utils/get_rclone.sh
+++ utils/get_rclone.sh
@@ -3,9 +3,8 @@
 # get latest rclone
 
 # curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
-# unzip rclone-current-linux-amd64.zip
-curl -L https://github.com/rclone/rclone/releases/download/v1.58.1/rclone-v1.58.1-linux-amd64.zip
-unzip rclone-v1.58.1-linux-amd64.zip
+curl -L -o rclone-current-linux-amd64.zip https://github.com/rclone/rclone/releases/download/v1.58.1/rclone-v1.58.1-linux-amd64.zip
+unzip rclone-current-linux-amd64.zip
 
 # find downloaded rclone
 rclone=$(realpath "$(find . -type f -name rclone)")
