86688a8ed31f07bdb8ab9ccb67e5a43d5e0b6d80
seals55 <seals55@users.noreply.github.com>
2022-06-27 20:35:12 -0400

    try again

--- copy/remotes/copy_remote_015.sh
+++ copy/remotes/copy_remote_015.sh
@@ -3,5 +3,5 @@
 # Comics
 base_src_dir="tower:/mnt/user/Comics"
 base_dest_dir="GoogleSealsTV_Encrypted:/Comics"
-dupe_dir="${base_dest_dir}/dupes"
+dupe_dir="GoogleSealsTV_Encrypted:/Comics_dupes"
 rclone_copy "${base_src_dir}" "${base_dest_dir}" "${dupe_dir}"
