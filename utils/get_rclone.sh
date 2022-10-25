c6b48e5ab3c9dd2d0a03746df30606c872520a9c
seals55 <seals55@users.noreply.github.com>
2022-10-25 14:21:01 -0400

    update to v1.60.0

--- utils/get_rclone.sh
+++ utils/get_rclone.sh
@@ -3,7 +3,7 @@
 # get latest rclone
 
 # curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
-curl -L -o rclone-current-linux-amd64.zip https://github.com/rclone/rclone/releases/download/v1.58.1/rclone-v1.58.1-linux-amd64.zip
+curl -L -o rclone-current-linux-amd64.zip https://downloads.rclone.org/v1.60.0/rclone-v1.60.0-linux-amd64.zip
 unzip rclone-current-linux-amd64.zip
 
 # find downloaded rclone
